ed355617df973880b56eccd513c27dddf9ceedbf
Berat Aktas <berataktas2834@gmail.com>
2024-12-12 23:49:40 +0300

    12.12.2024

--- CSharpEgitimKampi301.DataAccesLayer/EntityFramework/EfProductDal.cs
+++ CSharpEgitimKampi301.DataAccesLayer/EntityFramework/EfProductDal.cs
@@ -1,4 +1,5 @@
 ﻿using CSharpEgitimKampi301.DataAccesLayer.Abstract;
+using CSharpEgitimKampi301.DataAccesLayer.Context;
 using CSharpEgitimKampi301.DataAccesLayer.Repositories;
 using CSharpEgitimKampi301.EntityLayer.Concrete;
 using System;
@@ -11,5 +12,20 @@ namespace CSharpEgitimKampi301.DataAccesLayer.EntityFramework
 {
 	public class EfProductDal : GenericRepository<Product>, IProductDal
 	{
+		public List<Product> GetProductsWithCategory()
+		{
+			var context = new CampContext();
+			var values = context.Products
+				.Select(x=> new Product
+				{
+					ProductId = x.ProductId,
+					ProductName = x.ProductName,
+					ProductStock = x.ProductStock,
+					ProductPrice = x.ProductPrice,
+					ProductDescription = x.ProductDescription,
+					CategoryName = x.Category.CategoryName,
+				}).ToList();
+			return values;
+		}
 	}
 }
